68c1ba85992cfe261c37abc52628ef8a7e8844cd
AleksandrEnin163 <alexenin163@gmail.com>
2023-12-20 21:10:20 +0100

    20.12.23

--- hw13.12.2023.js
+++ hw13.12.2023.js
@@ -1,12 +1,16 @@
 // 200
 function rgb(r, g, b) {
-  const rgbArr = [r, g, b]
-  return rgbArr
+  return [r, g, b]
     .map(value => value > 255 ? 255 : value)
     .map(value => value < 0 ? 0 : value)
-    .map(value => [Math.floor(value / 16), (value % 16)].map(num => num.toString(16).toUpperCase()).join(''))
-    .join('')           
+    // .map(value => [Math.floor(value / 16), (value % 16)].map(num => num.toString(16).toUpperCase()).join(''))
+    .map(value => value.toString(16).padStart(2, '0'))
+    .join('')
+    .toUpperCase()
 }
+
+console.log(">>>", rgb(255, 128, 33));
+console.log(">>>", rgb(255, 5, 33)); // FF0521
 
 // 201
 function twoSum(numbers, target) {
@@ -20,8 +24,11 @@ function twoSum(numbers, target) {
 }
 
 // 202
+
+const isNullOrEmpty = arr => arr === null || arr.length === 0;
+
 function getLengthOfMissingArray(arrayOfArrays) {
-    if (arrayOfArrays === null || arrayOfArrays.length === 0 || arrayOfArrays.some(arr => arr === null || arr.length === 0)) {
+    if (isNullOrEmpty(arrayOfArrays) || arrayOfArrays.some(isNullOrEmpty)) {
       return 0;
     }
     
@@ -36,27 +43,31 @@ function getLengthOfMissingArray(arrayOfArrays) {
     const bytes = []
     const size = 8
     for(let i = 0; i < data.length; i += size){
-      bytes.unshift(data.slice(i, i + size))
+      bytes.push(data.slice(i, i + size))
     }
-    return bytes.flat()
+    return bytes.reverse().flat()
   }
 
 //   204
 function proofread (str) { 
     return str
-      .split('. ')
-      .map(string => string[0].toUpperCase() + string.slice(1).toLowerCase())
-      .join('. ')
+      .toLowerCase()
       .replaceAll('ie', 'ei')
+      .split('. ')
+      .map(string => string[0].toUpperCase() + string.slice(1))
+      .join('. ')
   }
+
+  // Iefytirdirt
 
   // 206
   function deleteDigit(n) {
     const numbers = n.toString().split('')
     const newNumbers = []
     for(let i = 0; i < numbers.length; i++){
-      let numbersCopy = [...numbers]
-      numbersCopy.splice(i, 1)
+      // const numbersCopy = [...numbers]
+      // numbersCopy.splice(i, 1)]
+      const numbersCopy = numbers.toSpliced(i, 1);
       newNumbers.push(parseInt(numbersCopy.join('')))
     }
     return Math.max(...newNumbers)
@@ -68,29 +79,88 @@ function sumArray(array){
   }
   
   function findEvenIndex(arr){
-    const indexes = []
-    if(sumArray(arr.slice(1)) === 0){
-      return 0
-    }
-    for(let i = 1; i < arr.length; i++){
+    // if(sumArray(arr.slice(1)) === 0){
+    //   return 0
+    // }
+    for(let i = 0; i < arr.length; i++){
       if(sumArray(arr.slice(0, i)) === sumArray(arr.slice(i + 1))){
-        indexes.push(i)
-      }
-    }
-    return indexes.length === 0 ? -1 : Math.min(...indexes)
-  }
-
-  // 209
-  function indexEqualsValue(a) {
-    for(let i = 0; i < a.length; i++){
-      if(a[i] === i){
         return i
       }
     }
     return -1
-    
-  //   return a.indexOf(a.find((value, i) => value === i)) 
   }
+
+  console.log(findEvenIndex([2, 5, 1, 1])) // 1 ???
+  console.log(findEvenIndex([5, -2, 1, 1])) // 0 ???
+  console.log(findEvenIndex([5, -2, 1])) // -1 ???
+
+  // 209
+  function indexEqualsValue(a) {
+    // for(let i = 0; i < a.length; i++){
+    //   if(a[i] === i){
+    //     return i
+    //   }
+    // }
+    // return -1
+    
+    return a.findIndex((value, i) => value === i)
+  }
+
+  //             0    1  2  3  4  5   6   7   8    9   10   11    12    13      14    15
+  const arr = [-55, -10, 0, 1, 4, 6, 12, 22, 34, 100, 200, 300, 3000, 9999, 789787];
+  //             L                        M                                            R
+
+
+  // * + 4 / - * 7 + 3 / + 3 5 * 2 2 2 11 3
+  // * + 4 / - * 7 + 3 / 8 * 2 2 2 11 3
+  // * + 4 / - * 7 + 3 / 8 4 2 11 3
+  // * + 4 / - * 7 + 3 2 2 11 3
+  // * + 4 / - * 7 5 2 11 3
+  // * + 4 / - 35 2 11 3
+  // * + 4 / 33 11 3
+  // * + 4 3 3
+  // * 7 3
+  // 21
+
+  const operators = {
+    "+": (a, b) => a + b,
+    "/": (a, b) => a / b,
+    "-": (a, b) => a - b,
+    "*": (a, b) => a * b,
+  }
+
+  function calculate(expression) {
+    const tokens = expression.split(' ')
+
+    for(let i = 0; i < tokens.length; i ++){
+      if(operators.hasOwnProperty(tokens[i]) && !operators.hasOwnProperty(tokens[i + 1]) && !operators.hasOwnProperty(tokens[i + 2])){
+        const result = operators[tokens[i]](+tokens[i + 1], +tokens[i + 2]);
+
+        console.log(operators[tokens[i]]);
+        console.log(result);
+        break;
+      }
+    }
+    // for → findIndex
+    // + 3 5 → 8
+    // заменяем эти 3 элемента на 8
+
+    // повторяем в цикле
+  }
+
+  calculate('* + 4 / - * 7 + 3 / + 3 5 * 2 2 2 11 3')
+
+
+  
+
+
+
+
+
+
+
+  // 
+  // 
 
   // 214
   function zeroPlentiful(arr){
