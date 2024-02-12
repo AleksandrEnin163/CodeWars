c045c0abbe76e8d0ccb6e4c3aa7c2fe8a621d25b
AleksandrEnin163 <alexenin163@gmail.com>
2024-02-12 21:26:45 +0100

    12/02/24/(2)

--- FP.js
+++ FP.js
@@ -322,5 +322,26 @@ function join(initialStr) {
   return innerJoin;
 }
 
+// Склонение существительных
+// Реализуйте функцию, которая позволяет склонять существительное во множественном числе.
+
+// Например, 1 корова, 2 коровы, 5 коров, 24 коровы, 51 корова. Или 2 задачи, 70 задач и 21 задача.
+
+function plural(words) {
+  return function(count) {
+      let wordIndex;
+      if (count % 10 === 1 && count % 100 !== 11) {
+          wordIndex = 0;
+      } else if (count % 10 >= 2 && count % 10 <= 4 && (count % 100 < 10 || count % 100 >= 20)) {
+          wordIndex = 1;
+      } else if(count === 0){
+          wordIndex = 2;
+      }else{
+          wordIndex = 2
+      }
+      return words[wordIndex];
+  };
+}
+
 
 
